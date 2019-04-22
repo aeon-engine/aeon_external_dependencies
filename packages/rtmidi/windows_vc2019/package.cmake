3070bf073d0bedbd78ab35dd1875c4f2099efb55
Robin Degen <robindegen@gmail.com>
2019-04-22 10:11:11 +0200

    From rtmidi 4.0.0 onward, the package is build as a shared dll, so copy the dlls to the runtime directory

--- packages/rtmidi/windows_vc2019/package.cmake
+++ packages/rtmidi/windows_vc2019/package.cmake
@@ -9,4 +9,7 @@ if (NOT TARGET RTMidi::RTMidi)
 
     set_property(TARGET RTMidi::RTMidi APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
     set_target_properties(RTMidi::RTMidi PROPERTIES IMPORTED_LOCATION_DEBUG "${CMAKE_CURRENT_LIST_DIR}/lib/rtmidid.lib")
+
+    copy_files_to_runtime_path(FILES "${CMAKE_CURRENT_LIST_DIR}/bin/rtmidi.dll" CONFIGURATIONS Release)
+    copy_files_to_runtime_path(FILES "${CMAKE_CURRENT_LIST_DIR}/bin/rtmidid.dll" CONFIGURATIONS Debug)
 endif ()
