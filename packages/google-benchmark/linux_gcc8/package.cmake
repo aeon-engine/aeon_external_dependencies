14e05fb6cbd21e0f01b25893b720040c2b2c7a61
Robin Degen <robindegen@gmail.com>
2019-04-22 10:34:05 +0200

    Add package config for google-benchmark on linux_gcc8

---
+++ packages/google-benchmark/linux_gcc8/package.cmake
@@ -0,0 +1,12 @@
+if (NOT TARGET GoogleBenchmark::GoogleBenchmark)
+    add_library(GoogleBenchmark::GoogleBenchmark STATIC IMPORTED)
+    set_target_properties(GoogleBenchmark::GoogleBenchmark PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include")
+    set_target_properties(GoogleBenchmark::GoogleBenchmark PROPERTIES IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/lib/libbenchmark.a")
+endif ()
+
+if (NOT TARGET GoogleBenchmark::Main)
+    add_library(GoogleBenchmark::Main STATIC IMPORTED)
+    set_target_properties(GoogleBenchmark::Main PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include")
+    set_target_properties(GoogleBenchmark::Main PROPERTIES IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/lib/libbenchmark_main.a")
+    set_target_properties(GoogleBenchmark::Main PROPERTIES INTERFACE_LINK_LIBRARIES GoogleBenchmark::GoogleBenchmark)
+endif ()
