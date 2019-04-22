6d0aa2ce167bf122c18e37839379dd959046fa60
Robin Degen <robindegen@gmail.com>
2019-04-22 10:40:40 +0200

    Add package config for freetype2 on linux_gcc8

---
+++ packages/freetype2/linux_gcc8/package.cmake
@@ -0,0 +1,10 @@
+# Copyright (c) 2012-2019 Robin Degen
+
+include (CopyToRuntimePath)
+
+if (NOT TARGET Freetype::Freetype)
+    add_library(Freetype::Freetype STATIC IMPORTED)
+    set_target_properties(Freetype::Freetype PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include/freetype2")
+    set_target_properties(Freetype::Freetype PROPERTIES IMPORTED_LOCATION_RELEASE "${CMAKE_CURRENT_LIST_DIR}/lib/libfreetype.a")
+    set_target_properties(Freetype::Freetype PROPERTIES INTERFACE_LINK_LIBRARIES PNG::PNG)
+endif ()
