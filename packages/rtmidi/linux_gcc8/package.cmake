637c05ff66423e4338d2c7b35e4c3e54d7f30164
Robin Degen <robindegen@gmail.com>
2019-04-22 10:34:42 +0200

    Add package config for rtmidi on linux_gcc8

---
+++ packages/rtmidi/linux_gcc8/package.cmake
@@ -0,0 +1,9 @@
+# Copyright (c) 2012-2019 Robin Degen
+
+if (NOT TARGET RTMidi::RTMidi)
+    add_library(RTMidi::RTMidi STATIC IMPORTED)
+    set_target_properties(RTMidi::RTMidi PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include")
+    set_target_properties(RTMidi::RTMidi PROPERTIES IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/lib/librtmidi.so")
+
+    copy_files_to_runtime_path(FILES "${CMAKE_CURRENT_LIST_DIR}/lib/librtmidi.so")
+endif ()
