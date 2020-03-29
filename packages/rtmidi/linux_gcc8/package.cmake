9fd225fc85c9dfe419aa3d48a00cfb48b1e3965d
Robin Degen <robindegen@gmail.com>
2020-03-29 16:52:18 +0200

    Fix rtmidi using librtmidi.so.5 rpath on linux

--- packages/rtmidi/linux_gcc8/package.cmake
+++ packages/rtmidi/linux_gcc8/package.cmake
@@ -5,5 +5,5 @@ if (NOT TARGET RTMidi::RTMidi)
     set_target_properties(RTMidi::RTMidi PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include")
     set_target_properties(RTMidi::RTMidi PROPERTIES IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/lib/librtmidi.so")
 
-    copy_files_to_runtime_path(FILES "${CMAKE_CURRENT_LIST_DIR}/lib/librtmidi.so")
+    copy_files_to_runtime_path(FILES "${CMAKE_CURRENT_LIST_DIR}/lib/librtmidi.so.5")
 endif ()
